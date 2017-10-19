2700280d6872f985489cfacb59f3c93225a15c58
Paul Quinnell <quin0234@algonquinlive.com>
2017-10-19 12:19:40 -0400

    first commit
    first commit

---
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.sriyanksiddhartha.stylesandthemesdemo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+
+public class SecondActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			Explode enterTransition = new Explode();
+			enterTransition.setDuration(1000);
+			getWindow().setEnterTransition(enterTransition);
+		}
+	}
+}
